393820311379ed061236341e72e9c6a69e17de62
zhazhaxia <337262356@qq.com>
2017-09-09 13:20:14 +0800

    test

--- webaudio/public/js/recordsong.js
+++ webaudio/public/js/recordsong.js
@@ -53,7 +53,7 @@ define('./js/recordsong',function( require, exports, module){
         	exports.mediaRecorder.onstop = function (e) {
       			var blob = new Blob(exports.chunk, { 'type' : 'audio/wav;' }),
       			    url = window.URL.createObjectURL(blob);
-                alert(url)
+                alert(blob.size)
         		exports.audio.src = url;
         	}
         	exports.mediaRecorder.ondataavailable = function (e) {
